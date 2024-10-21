8a508aeefcace593f72b3219920535bc6906860c
Kamal-Baraili <kamalbaraili86@gmail.com>
2024-10-21 16:07:48 +0545

    Props

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import ClassComp from "./components/Greeting";
 function App() {
   return (
     <div className="App">
-      <MyComp name="Kamal" />
-      <MyComp name="John" />
+      <MyComp name="Kamal">This is a children props</MyComp>
+      <MyComp name="John">
+        <button>Action</button>
+      </MyComp>
+
       <MyComp name="Mark" />
-      <ClassComp />
+      <ClassComp name="Kamal" />
+      <ClassComp name="John" />
+      <ClassComp name="Mark" />
     </div>
   );
 }
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,5 +1,10 @@
 const Greet = (props) => {
-  return <h1>Hello {props.name}</h1>;
+  return (
+    <div>
+      <h1>Hello {props.name}</h1>
+      {props.children}
+    </div>
+  );
 };
 
 export default Greet;
