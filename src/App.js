b7286cd95c2c20e07d5fe50c37779bc2a11cb812
Kamal-Baraili <kamalbaraili86@gmail.com>
2024-10-21 15:08:44 +0545

    Class Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import MyComp from "./components/Hello";
+import ClassComp from "./components/Greeting";
 
 function App() {
   return (
     <div className="App">
       <MyComp />
+      <ClassComp />
     </div>
   );
 }
